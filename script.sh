af198b4f5dd97a3fd30a8c04ba4f25335ec60024
vineetpatel2611 <52060953+vineetpatel2611@users.noreply.github.com>
2020-01-27 20:51:02 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,4 @@
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
